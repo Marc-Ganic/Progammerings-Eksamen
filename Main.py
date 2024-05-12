b8949f74ef2c7575ac1be87cb3a4ab65d9734a08
Marc-G <marcganic@gmail.com>
2024-05-12 20:31:42 +0200

    Set up flask & a quick display change for the front page and the game page

--- Main.py
+++ Main.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template, request
 from waitress import serve
 
 app = Flask(__name__)
+print("Starting...")
 
 @app.route('/')
 @app.route('/index')
-@app.route('/template')
-
 def index():
-  return "Hello World!!!".(
+  name = request.args.get('name')
+  if name == "Play!": # name should be the same as name of Gamebtn in index.html
+    return render_template("template.html")
+  else:
+    return render_template("index.html")
+
+
+@app.route('/template')
+def template():
+  return render_template("template.html")
 
 if __name__ == "__main__":
-  serve(app, host="0.0.0.0", port=8000)
+  serve(app, host="0.0.0.0", port=8888)
